fb1bf561080bb9d2b04fe1ddb820f56db4b4eb42
yash <yashnariya3364@gmail>
2026-04-06 17:02:47 +0530

    change style.scss in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './Style.scss';
+import './style/Style.scss';
 
 
 createRoot(document.getElementById('root')).render(
